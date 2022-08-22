ba12100db0f42d62c433d2156fecbea8682a67a3
akshatcoder-hash <akshat@shastraos.co>
2022-08-22 11:54:34 +0530

    Created asynchronous main

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,5 +2,9 @@ const main = async () = {
   try {
     const nftContractFactory = await Headers.ethers.getContractFactory("ChainBattles")
     const nftContract = await nftContractFactory.deploy();
+    await.nftContract.deployed();
+
+    console.log("Contract Deployed to :", nftContract.address);
+    process.exit(0);
   } catch (error)
 }
